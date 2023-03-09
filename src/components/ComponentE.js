95191ecb5918d7863d08b0d15ba8942ecc4b79d1
fazal <fazal@lotusinsights.in>
2023-03-09 12:38:01 +0530

    first

---
+++ src/components/ComponentE.js
@@ -0,0 +1,20 @@
+import React,{useContext} from 'react'
+import ComponentF from './ComponentF'
+import { UserContext,ChannelContext } from '../App'
+
+function ComponentE() {
+
+
+    const user = useContext(UserContext)
+    const channel = useContext(ChannelContext)
+
+  return (
+    <div>
+        <ComponentF/>
+        
+        <h1>{user} - {channel} </h1>
+    </div>
+  )
+}
+
+export default ComponentE
